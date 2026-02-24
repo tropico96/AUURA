c232a8a34a9c60acb77027187cfd5de3998ae79e
tropico96 <pipialuca1@gmail.com>
2026-02-24 21:26:31 +0100

    Add AURA project files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// DOM Elements
+const content = document.getElementById('content');
+
+// Initialize
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('AUURA Project Loaded');
+    initializeApp();
+});
+
+// Initialize Application
+function initializeApp() {
+    console.log('App initialized');
+}
